3f1583a49032baa0f0d8c9ae732c56f964055c75
choudharymayank29 <mayankror27@gmail.com>
2026-02-07 20:42:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let div = document.createElement('div');
+
+
+div.id = "paragraph";
+div.className = "alert";
+
+
+div.innerHTML = "<p>Congrats! You have created a div.</p>";
+
+
+
+
+document.body.append(div);
+
+
+
+
+
+let heading = document.getElementById("welcome");
+heading.innerHTML = '&#128512;'
